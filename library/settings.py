e4bacc72eb81886aeb6da3ea9d30ef8589deb402
bohdanshlyah <bohdanshlyah@gmail.com>
2021-11-08 14:26:48 +0200

    Added files and changes required for deployment to heroku

--- library/settings.py
+++ library/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'q!o9ybrsa@o8a)fj2&d7qrbtnbvkej5mud(!k)*9vspug62$xh'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'q!o9ybrsa@o8a)fj2&d7qrbtnbvkej5mud(!k)*9vspug62$xh')
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'q!o9ybrsa@o8a)fj2&d7qrbtnbvkej5mud(!k)*9vspug62$xh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -56,12 +58,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'library.urls'
@@ -72,8 +76,7 @@ TEMPLATES = [
         'DIRS': [
             Path.joinpath(BASE_DIR, 'library', 'templates'),
             Path.joinpath(BASE_DIR, 'templates'),
-            ]
-        ,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'library', 'static', 'css'),
     Path.joinpath(BASE_DIR, 'library', 'static'),
@@ -151,6 +155,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 try:
-    from .local_settings import  *
+    from .local_settings import *
 except ImportError:
     pass
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
